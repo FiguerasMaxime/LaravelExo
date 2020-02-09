7a767682b618438c5d63927d4c6107f516e9ed4b
FiguerasMaxime <maxime.figueras@gmail.com>
2020-02-09 19:31:38 +0100

    :rocket: Deploy Laravel

---
+++ app/Comment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    //
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content'
+    ];
+
+
+    /**
+     * Get the user that owns the comment.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the comments that owns the comment.
+     */
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
+    protected $table = "comments";
+}
---
+++ app/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'content'
+    ];
+
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
+    protected $table = "posts";
+
+}
